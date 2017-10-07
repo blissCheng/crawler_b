c3c47787a78a90bd98143170e10be0516490c9ca
cheng-xunsi <1032211967@qq.com>
2017-10-07 18:14:11 +0800

    sql

--- src/config.js
+++ src/config.js
@@ -1,8 +1,15 @@
-
+import mysql from 'mysql'
 const config = {
     common: 'http://api.bilibili.com',
     comment: 'http://api.bilibili.com/archive_rank/getarchiverankbypartion?type=jsonp',
     tId: [22,26,126]
 }
+config.connection = mysql.createConnection({
+    host:     'localhost',
+    user:     'root',
+    password: '960810',
+    database: 'bilibili',
+})
+
 
 export { config }
